7a4887aab8c31f97c649d628f8c4d45ba37bc073
Cisner0s <dcisnm00@estudiante.unileon.es>
2023-11-20 22:51:33 +0100

    Structure of the practice

---
+++ src/MainFichasIguales.java
@@ -0,0 +1,29 @@
+// Prueba GitHub
+// Cambios de prueba 
+//    Clase JuegoFichas:
+    //     Atributos: Número de filas, número de columnas, matriz del tablero.
+    //     Métodos:
+    //         Método para cargar la configuración inicial del tablero.
+    //         Método para imprimir el estado actual del tablero.
+
+    // Clase Movimiento:
+    //     Atributos: Número de movimiento, coordenadas (fila, columna), número de fichas eliminadas, color de las fichas, puntuación.
+    //     Método para imprimir la información del movimiento.
+
+    // Clase EstrategiaOptima:
+    //     Atributos: Instancia del juego actual.
+    //     Método para encontrar el conjunto de movimientos que da la mayor puntuación posible.
+
+    // Clase Principal:
+    //     Método main que maneje la entrada y salida del programa.
+    //     Crear instancias de las clases anteriores y llamar a los métodos correspondientes para ejecutar el juego.
+
+    package src;
+
+    public class MainFichasIguales{
+        public static void main(String[] args) throws Exception {
+
+        }
+
+
+    }
